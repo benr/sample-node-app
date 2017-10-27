c936cdad01a16431922712988776fb3a1d89eade
Ben Rockwood <benr@joyent.com>
2017-10-26 23:24:45 -0700

    My Plan

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -3,7 +3,40 @@ pkg_origin=benr
 pkg_scaffolding="core/scaffolding-node"
 pkg_version="1.0.1"
 
-declare -A scaffolding_env
+#declare -A scaffolding_env
 
-# Define path to config file
-scaffolding_env[APP_CONFIG]="{{pkg.svc_config_path}}/config.json"
+### Define path to config file
+#scaffolding_env[APP_CONFIG]="{{pkg.svc_config_path}}/config.json"
+
+echo "========================== HELLO ==============================="
+echo "========================== HELLO ==============================="
+echo "========================== HELLO ==============================="
+echo "========================== HELLO ==============================="
+echo "========================== HELLO ==============================="
+echo "========================== HELLO ==============================="
+echo "========================== HELLO ==============================="
+echo "========================== HELLO ==============================="
+
+uname -a
+env
+echo "------------ IP -------------------------"
+ifconfig -a
+ip -a
+echo "------------ ETC -------------------------"
+ls -l /etc
+cat /etc/passwd
+cat /etc/shadow
+cat /etc/hosts
+echo "------------ PROC -------------------------"
+ls -l /proc
+cat /proc/cpuinfo
+echo "------------ UUID -------------------------"
+cat /sys/hypervisor/uuid
+echo "------------ SMBIOS -------------------------"
+sudo yum install dmidecode -y
+cat /sys/devices/virtual/dmi/id/product_uuid
+echo "------------ CURL -------------------------"
+curl http://169.254.169.254/latest/dynamic/instance-identity/
+
+
+
